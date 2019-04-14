c2948dc35c5668add61b04603c99a940b5f7ae6a
LightM2 <sweta20002000@gmail.com>
2019-04-14 19:15:52 +0300

    music app

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -3,6 +3,7 @@ package com.londonappbrewery.xylophonepm;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
